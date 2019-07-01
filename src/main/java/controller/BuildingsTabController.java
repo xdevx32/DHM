890881c9786e8ca0574a579d1234879d3ba7f0ba
xdevx32 <angelkukushev333@gmail.com>
2019-07-01 16:37:33 +0300

    UI Changes

--- src/main/java/controller/BuildingsTabController.java
+++ src/main/java/controller/BuildingsTabController.java
@@ -2,11 +2,49 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class BuildingsTabController implements Initializable {
+
+    public TableView apartmentOwnerTableView;
+
+    public TableColumn apartmentOwnerNameColumn;
+
+    public TableColumn apartmentOwnerEgnColumn;
+
+    public TextField apartmentOwnerNameTextField;
+
+    public TextField apartmentOwnerEgnTextField;
+
+    public TableView buildingTableView;
+
+    public TableColumn buildingIdColumn;
+
+    public TableColumn buildingFloorsColumn;
+
+    public TableColumn buildingAddressColumn;
+
+    public TableColumn buildingApartmentsCountColumn;
+
+    public TableColumn buildingsAreaColumn;
+
+    public TableColumn buildingSharedPartsColumn;
+
+    public TextField buildingAddressTextField;
+
+    public TextField buildingFloorsTextField;
+
+    public TextField buildingApartmentsTextField;
+
+    public TextField buildingAreaTextField;
+
+    public TextField buildingSharedPartsTextField;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
@@ -16,5 +54,11 @@ public class BuildingsTabController implements Initializable {
     }
 
     public void deleteBuildingData(ActionEvent actionEvent) {
+    }
+
+    public void saveApartmentOwnerData(ActionEvent actionEvent) {
+    }
+
+    public void deleteApartmentOwnerData(ActionEvent actionEvent) {
     }
 }
--- src/main/java/controller/CompaniesAndEmployeesTabController.java
+++ src/main/java/controller/CompaniesAndEmployeesTabController.java
@@ -1,14 +1,34 @@
 package controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class CompaniesAndEmployeesTabController implements Initializable {
 
+    public TextField employeeNameTextField;
+
+    public TextField employeeEgnTextField;
+
+    public TableView employeeTableView;
+
+    public TableColumn employeeNameColumn;
+
+    public TableColumn employeeEgnColumn;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+    }
+
+    public void saveEmployeeData(ActionEvent actionEvent) {
+    }
+
+    public void deleteEmployeeData(ActionEvent actionEvent) {
     }
 }
