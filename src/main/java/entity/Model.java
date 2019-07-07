1dd2374d4ab3e2fcf5c066d4c8187ec7198ea321
xdevx32 <angelkukushev333@gmail.com>
2019-07-07 09:21:49 +0300

    Model class and methods

--- src/main/java/entity/Model.java
+++ src/main/java/entity/Model.java
@@ -1,6 +1,45 @@
 package entity;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Model {
+
+    // Lists and ObservableLists
+
+    /*
+    *
+    * Buildings
+    *
+     */
+    ObservableList<Building> buildingsObservableList = FXCollections.observableArrayList();
+
+    List<Building> buildingsList = DBMethods.getBuildings();
+
+
+    /*
+    *
+    * Buildings for an employee
+    *
+     */
+    ObservableList<Building> buildingsForEmployeeOL = FXCollections.observableArrayList();
+
+    List<Building> buildingsForEmployeeList = new ArrayList<Building>(0);
+
+    /*
+    *
+    * Companies
+    *
+    */
+
+    ObservableList<Company> companiesObservableList = FXCollections.observableArrayList();
+
+    List<Company> companiesList = DBMethods.getCompanies();
+
 
     // static variable single_instance of type Model
     private static Model single_instance = null;
@@ -12,6 +51,67 @@ public class Model {
             single_instance = new Model();
 
         return single_instance;
+    }
+
+    public Model() {
+        buildingsObservableList.setAll(DBMethods.getBuildings());
+        companiesObservableList.setAll(DBMethods.getCompanies());
+    }
+
+    public ObservableList<Building> getBuildingsObservableList() {
+        return buildingsObservableList;
+    }
+
+    public ObservableList<Company> getCompaniesObservableList() {
+        return companiesObservableList;
+    }
+
+    public void setBuildingsObservableList(ArrayList<Building> buildingsList) {
+        buildingsObservableList.setAll(buildingsList);
+    }
+
+    public void setCompaniesObservableList(ArrayList<Company> companiesArrayList) {
+        companiesObservableList.setAll(companiesArrayList);
+    }
+
+    public ObservableList<Building> getBuildingsForEmployeeOL() {
+        return buildingsForEmployeeOL;
+    }
+
+    public List<Building> getBuildingsList() {
+        return buildingsList;
+    }
+
+    public List<Company> getCompaniesList() {
+        return companiesList;
+    }
+
+    public void setBuildingsForEmployeeOL(List<Building> buildingsArrayList) {
+        buildingsForEmployeeOL.setAll(buildingsArrayList);
+    }
+
+    public void setBuildingsList(List<Building> buildingsList) {
+        this.buildingsList = buildingsList;
+    }
+
+    public void setCompaniesList(List<Company> companiesList) {
+        this.companiesList = companiesList;
+    }
+
+    public void addBuildingToArrayList(Building building) {
+        this.buildingsList.add(building);
+    }
+
+    public void removeBuildingFromArrayList(Building building) {
+        this.buildingsList.remove(building);
+    }
+
+    public void addCompanyToArrayList(Company company) {
+        this.companiesList.add(company);
+    }
+
+    public void removeCompanyFromArrayList(Company company) {
+        this.companiesList.remove(company);
     }
 
 }
